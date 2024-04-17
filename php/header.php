9337a2e05ba3fc4027d624a2c531f798cb4973d0
jrp03 <158624512+jrp03@users.noreply.github.com>
2024-04-16 21:21:37 -0700

    Add files via upload

--- php/header.php
+++ php/header.php
@@ -7,9 +7,24 @@
             <ul>
                 <li><a href="">Inicio</a></li>
                 <li><a href="">Pedidos</a></li>
-                <li><a href="index.html">Inicia Seción</a></li>
-                <li><a href="registrarse.html">Registrar</a></li>
+                <li><a href="login.php">Inicia Seción</a></li>
+                <li><a href="registrarse.php">Registrar</a></li>
             </ul>
        </div>
     </header>
+    <div class="btm-header">
+        <div class="btm-header-content">
+            <p>Sucursal</p>
+            <input hidden class="sucs" type="checkbox" name="" id="sucs">
+            <label for="sucs" id="">
+                <p class="btn" id="elejir">Elejir</p>
+            </label>
+            
+            <ul class="btm-suc">
+                <li><a href="">C.P Independencia #10</a></li>
+                <li><a href="">C.P Independencia #11</a></li>
+                <li><a href="">C.P Independencia #12</a></li>
+            </ul>
+        </div>
+    </div>
 </header>
