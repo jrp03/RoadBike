b749c90c667754168cd3135bf7542a2df026ad85
jrp03 <158624512+jrp03@users.noreply.github.com>
2024-04-18 22:58:44 -0700

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet">
     <link rel="stylesheet" href="css/registers.css">
     <link rel="stylesheet" href="css/def.css">
     <link rel="stylesheet" href="css/style.css">
