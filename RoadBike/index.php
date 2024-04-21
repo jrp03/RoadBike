17fd9a6ed23e94561cd7d34d9bcf68502abd0166
ferxeiwo <161990341+ferxeiwo@users.noreply.github.com>
2024-04-20 20:50:27 -0700

    Add files via upload

---
+++ RoadBike/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RoadBike</title>
+     <!-- icon-->
+     <link rel="icon" href="icon/icon.ico.ico">
+    <!-- css -->
+    <link rel="stylesheet" href="css/def.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+        
+    <?php require_once "php/header.php"; ?>
+
+    
+
+        <div class="fondo-img">
+                <img src="recursos-img/bici-footer3.jpg" alt="">
+            </div>
+
+        <div class="banner">
+            <div class="title">
+                <h1>RoadBike, Tienda De Bicicletas</h1>
+            </div>
+            <div class="r-c-banner">
+                <a href="">¡Lo Más Nuevo!</a>
+            </div>
+            <div class="banner-footer">
+                <!-- <a href="">Sobre El sitio</a> -->
+                <div class="cards-banner">
+                    <a href="taller">
+                        taller de bici
+                    </a>
+                    <a href="coponentes">
+                        componentes
+                    </a>
+                    <a href="coponentes">
+                        sobre nosotros
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <main class="main">
+            <div class="main-content">
+                <section>
+                    <div>
+
+                    </div>
+                </section>
+                <div class="container">
+                    <div class="container-prin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo culpa totam, deserunt quibusdam facilis quidem placeat possimus dolorum? Obcaecati quod non similique minus vel doloremque repellendus sapiente repellat molestias officiis!</div>
+                    <div>b</div>
+                    <div>c</div>
+                    <div>d</div>
+                </div>
+            </div>
+        </main>
+
+
+        <footer>
+            <div class="footer-content">
+                <div class="card-footer">
+
+                </div>
+            </div>
+        </footer>
+   
+
+    
+        <script src="js/script.js"></script>
+</body>
+</html>
---
+++ RoadBike/php/header.php
@@ -0,0 +1,31 @@
+<header class="header-container">
+    <header class="header">
+       <div class="logo">
+            <a href="#">RoadBike</a>
+       </div>
+       <div class="nav">
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Pedidos</a></li>
+                <li><a href="login.php">Inicia Seción</a></li>
+                <li><a href="registrarse.php">Registrar</a></li>
+                <li><a href="agenda.html">agenda</a></li>
+            </ul>
+       </div>
+    </header>
+    <div class="btm-header">
+        <div class="btm-header-content">
+            <p>Sucursal</p>
+            <input hidden class="sucs" type="checkbox" name="" id="sucs">
+            <label for="sucs" id="">
+                <p class="btn" id="elejir">Elejir</p>
+            </label>
+            
+            <ul class="btm-suc">
+                <li><a href="">C.P Independencia #10</a></li>
+                <li><a href="">C.P Independencia #11</a></li>
+                <li><a href="">C.P Independencia #12</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
