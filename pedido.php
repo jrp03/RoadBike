015340327492a15d6ed548ac5cca39710f8bba68
jrp03 <158624512+jrp03@users.noreply.github.com>
2024-04-26 21:08:07 -0700

    updated 26/04/23 09:07

---
+++ pedido.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icon/icon.ico" type="image/x-icon">
+    <title>Document</title>
+    <!-- css -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/def.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+        
+    <?php require_once "php/header.php"; ?>
+
+    
+
+        <div class="fondo-img">
+            <img src="recursos-img/bici-footer3.jpg" alt="">
+        </div>
+        
+       <script src="js/jquery.js"></script>
+       <script src="js/script.js"></script>
+</body>
+</html>
