267526468254586ad4dceba8ccf011a8fb38337f
jrp03 <158624512+jrp03@users.noreply.github.com>
2024-05-20 23:03:57 -0700

    Add files via upload

---
+++ compra.php
@@ -0,0 +1,270 @@
+<?php
+    $page_section = "carrito.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icon/icon.ico" type="image/x-icon">
+    <title>Confirmar Compra</title>
+    <!-- css -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/def.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/footer.css">
+
+    <link rel="stylesheet" href="css/pedido.css">
+</head>
+<body>
+        
+    <?php require_once "php/header.php"; ?>
+    
+    <div class="main">
+        <div class="main-content">
+            
+            <div class="card-form">
+                <div class="form-card">
+                    <div class="form-left">
+                        <div class="card-title">
+                            <h1>Articulos (1)</h1>
+                        </div>
+                        
+                        <div class="productos">
+                        
+                                <div class="box-product">
+                                    <div class="box-prodcut-content">
+                                        <div class="img">
+                                            <img src="recursos-img/bici-electrica.jpg" alt="">
+                                        </div>
+                                        <div class="box-product-info">
+                                            <div class="box-product-title">
+                                                Bisicleta
+                                            </div>
+                                            <div class="box-product-title">
+                                                Marca mercurio
+                                            </div>
+                                            <div class="box-product-descr">
+                                                color: blanco, tamaño 120cm x 50cm
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-product-price">
+                                        $1,250
+                                    </div>
+                                </div>
+                                <div class="box-product">
+                                    <div class="box-prodcut-content">
+                                        <div class="img">
+                                            <img src="recursos-img/bici-electrica.jpg" alt="">
+                                        </div>
+                                        <div class="box-product-info">
+                                            <div class="box-product-title">
+                                                Bisicleta
+                                            </div>
+                                            <div class="box-product-title">
+                                                Marca mercurio
+                                            </div>
+                                            <div class="box-product-descr">
+                                                color: blanco, tamaño 120cm x 50cm
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-product-price">
+                                        $1,250
+                                    </div>
+                                </div>
+                                <div class="box-product">
+                                    <div class="box-prodcut-content">
+                                        <div class="img">
+                                            <img src="recursos-img/bici-electrica.jpg" alt="">
+                                        </div>
+                                        <div class="box-product-info">
+                                            <div class="box-product-title">
+                                                Bisicleta
+                                            </div>
+                                            <div class="box-product-title">
+                                                Marca mercurio
+                                            </div>
+                                            <div class="box-product-descr">
+                                                color: blanco, tamaño 120cm x 50cm
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-product-price">
+                                        $1,250
+                                    </div>
+                                </div>
+                                <div class="box-product">
+                                    <div class="box-prodcut-content">
+                                        <div class="img">
+                                            <img src="recursos-img/bici-electrica.jpg" alt="">
+                                        </div>
+                                        <div class="box-product-info">
+                                            <div class="box-product-title">
+                                                Bisicleta
+                                            </div>
+                                            <div class="box-product-title">
+                                                Marca mercurio
+                                            </div>
+                                            <div class="box-product-descr">
+                                                color: blanco, tamaño 120cm x 50cm
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-product-price">
+                                        $1,250
+                                    </div>
+                                </div>
+                                <div class="box-product">
+                                    <div class="box-prodcut-content">
+                                        <div class="img">
+                                            <img src="recursos-img/bici-electrica.jpg" alt="">
+                                        </div>
+                                        <div class="box-product-info">
+                                            <div class="box-product-title">
+                                                Bisicleta
+                                            </div>
+                                            <div class="box-product-title">
+                                                Marca mercurio
+                                            </div>
+                                            <div class="box-product-descr">
+                                                color: blanco, tamaño 120cm x 50cm
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-product-price">
+                                        $1,250
+                                    </div>
+                                </div>
+                                <div class="box-product">
+                                    <div class="box-prodcut-content">
+                                        <div class="img">
+                                            <img src="recursos-img/bici-electrica.jpg" alt="">
+                                        </div>
+                                        <div class="box-product-info">
+                                            <div class="box-product-title">
+                                                Bisicleta
+                                            </div>
+                                            <div class="box-product-title">
+                                                Marca mercurio
+                                            </div>
+                                            <div class="box-product-descr">
+                                                color: blanco, tamaño 120cm x 50cm
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-product-price">
+                                        $1,250
+                                    </div>
+                                </div>
+
+                                <div class="box-product">
+                                    <div class="box-prodcut-content">
+                                        <div class="img">
+                                            <img src="recursos-img/bici-electrica.jpg" alt="">
+                                        </div>
+                                        <div class="box-product-info">
+                                            <div class="box-product-title">
+                                                Bisicleta
+                                            </div>
+                                            <div class="box-product-title">
+                                                Marca mercurio
+                                            </div>
+                                            <div class="box-product-descr">
+                                                color: blanco, tamaño 120cm x 50cm
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="box-product-price">
+                                        $1,250
+                                    </div>
+                                </div>
+                        
+                        </div>
+
+                        <div class="form-banner-total">
+                            <table>
+                                <tr>
+                                    <td>Envio:</td>
+                                    <td class="Total">gratis</td>
+                                </tr>
+                                <tr >
+                                    <td>Total:</td>
+                                    <td style="color: green;">$1,250</td>
+                                </tr>
+                            </table>
+                        </div>
+
+                    </div>
+                    <div class="form-rigth">
+                        
+                        <div class="form-content">
+                            
+                            <div class="card-title">
+                                <h1>Confirmar Compra</h1>
+                            </div>
+
+                            <form action="" method="post">
+                                <div class="form-contact">
+                                    <div class="bx-1">
+                                        <h3>Datos de contactos</h3>
+                                        <div class="bx-1">
+                                            <!-- <p>Nombre</p> -->
+                                            <input type="text" placeholder="Nombre de quien Recibe" required>
+                                        </div>
+                                        <div class="bx-1">
+                                            <!-- <p>Apellido</p> -->
+                                            <input type="text" placeholder="Apellido" required>
+                                        </div>
+                                        <div class="bx-1">
+                                            <!-- <p>Apellido</p> -->
+                                            <input type="number" placeholder="Numero de celular" max="10" required>
+                                        </div>
+                                    </div>
+                                    <div class="bx-1">
+                                        <h3>Lugar de entrega</h3>
+                                        <div class="bx-2">
+                                            <input type="text" name="" id="" placeholder="Estado" required>
+                                            <input type="number" name="" id="" placeholder="codigo postal" required>
+                                        </div>
+                                        <div class="bx-2">
+                                            <input type="text" name="" id="" placeholder="Ciudad" required>
+                                        </div>
+                                        <div class="bx-2">
+                                            <textarea name="" id="" required placeholder="Calle, numero de casa, color de casa, calle, colonia, referencias"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="bx-1">
+                                        <h3>Datos de la tarjeta</h3>
+                                        <div class="bx-1">
+                                            <!-- <p>Número de tarjeta</p> -->
+                                            <input type="number" name="" id="" maxlength="16" minlength="16" placeholder="Número de tarjeta">
+                                        </div>
+                                        <div class="bx-2">
+                                            <input type="date" name="" id="">
+                                            <input type="number" name="" id="" placeholder="CVV">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-banner-total">
+                                    <input type="submit" value="Comprar">
+                                </div>
+                            </form>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+    </div>
+    <div class="fondo-img">
+        <img src="recursos-img/bici-footer3.jpg" alt="">
+    </div>
+        
+       <script src="js/jquery.js"></script>
+       <script src="js/script.js"></script>
+</body>
+</html>
--- php/header.php
+++ php/header.php
@@ -6,15 +6,49 @@
        </div>
        <div class="nav">
             <ul>
-
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="#">Pedidos</a></li>
-                <li><a href="login.php">Inicia Seción</a></li>
-                <li><a href="registrarse.php">Registrar</a></li>
+                <li>
+                    <a      <?php 
+                                if($page_section == "index.php"){ 
+                                    echo "class='active'"; 
+                            }?>
+                            href="index.php">
+                                Inicio
+                    </a>
+                </li>
+                <li>
+                    <a 
+                            <?php 
+                                if($page_section == "carrito.php"){ 
+                                    echo "class='active'"; 
+                            }?>
+                        href="carrito.php">
+                        Carrito
+                    </a>
+                </li>
+                <li>
+                    <a 
+                    <?php 
+                            if($page_section == "login.php"){ 
+                                    echo "class='active'"; 
+                            }?>
+                        href="login.php">
+                        Inicia Seción
+                    </a>
+                </li>
+                <li>
+                    <a      <?php 
+                                if($page_section == "registro.php"){ 
+                                    echo "class='active'"; 
+                            }?>
+                        
+                        href="registro.php">
+                        Registrar
+                    </a>
+                </li>
             </ul>
        </div>
     </header>
-    <div class="btm-header">
+    <!-- <div class="btm-header">
         <div class="btm-header-content">
             <p>Sucursal</p>
             <input hidden class="sucs" type="checkbox" name="" id="sucs">
@@ -22,11 +56,11 @@
                 <p class="btn" id="elejir">Elejir</p>
             </label>
             
-            <ul class="btm-suc">
-                <li><a href="">C.P Independencia #10</a></li>
-                <li><a href="">C.P Independencia #11</a></li>
-                <li><a href="">C.P Independencia #12</a></li>
-            </ul>
+                <ul class="btm-suc">
+                    <li><a href="">C.P Independencia #10</a></li>
+                    <li><a href="">C.P Independencia #11</a></li>
+                    <li><a href="">C.P Independencia #12</a></li>
+                </ul>
         </div>
-    </div>
+    </div> -->
 </header>
